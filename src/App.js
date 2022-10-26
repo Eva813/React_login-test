6c75fdb3c6aa3cccaf5cadbb887dfcf6db2d6b59
Eva <as45986@gmail.com>
2022-10-26 12:50:22 +0800

    react test 初探,以原本預設的範本來進行延伸學習

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login/Login';
 
 function App() {
+  const a = 2
+  const b = 1
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +20,13 @@ function App() {
         >
           Learn React
         </a>
+        <ul>
+          <li>Apple</li>
+          <li>Banana</li>
+          <li>Kiwi</li>
+        </ul>
+        <h1 data-testid="mytestid">Hello</h1>
+        <span title="sum">{a + b}</span>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,21 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders 3 list items', () => {
+  render(<App />);
+  const listItems = screen.getAllByRole("listitem");
+  expect(listItems.length).toEqual(3);
+});
+
+test('renders title', () => {
+  render(<App />);
+  const title = screen.getByTestId("mytestid");
+  expect(title).toBeInTheDocument();
+});
+
+test('sum should be 3', () => {
+  render(<App />);
+  const titleSum = screen.getByTitle("sum");
+  expect(titleSum.textContent).toBe("3");
+});
